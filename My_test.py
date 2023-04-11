b12ad34c3eea2f6c48b871017f15af3254aabe37
Emicora <emicoratolo2501@gmail.com>
2023-04-11 08:47:31 -0300

    Arreglo de test

--- My_test.py
+++ My_test.py
@@ -100,26 +100,43 @@ class TestDecimalToRomano(unittest.TestCase):
 
         self.assertEqual(resultado,"M")
         
-    def test_simple(self):
+    def test_simple1(self):
         self.assertEqual(roman_to_decimal("I"), 1)
+    def test_simple2(self):
         self.assertEqual(roman_to_decimal("V"), 5)
+    def test_simple3(self):
         self.assertEqual(roman_to_decimal("X"), 10)
+    def test_simple4(self):
         self.assertEqual(roman_to_decimal("L"), 50)
+    def test_simple5(self):
         self.assertEqual(roman_to_decimal("C"), 100)
+    def test_simple6(self):
         self.assertEqual(roman_to_decimal("D"), 500)
+    def test_simple7(self):
         self.assertEqual(roman_to_decimal("M"), 1000)
-    def test_suma(self):
+
+    def test_suma1(self):
         self.assertEqual(roman_to_decimal("II"), 2)
+    def test_suma2(self):
         self.assertEqual(roman_to_decimal("VI"), 6)
+    def test_suma3(self):
         self.assertEqual(roman_to_decimal("XIII"), 13)
+    def test_suma4(self):
         self.assertEqual(roman_to_decimal("LX"), 60)
+    def test_suma5(self):
         self.assertEqual(roman_to_decimal("MMXXI"), 2021)
-    def test_resta(self):
+
+    def test_resta1(self):
         self.assertEqual(roman_to_decimal("IV"), 4)
+    def test_resta2(self):
         self.assertEqual(roman_to_decimal("IX"), 9)
+    def test_resta3(self):
         self.assertEqual(roman_to_decimal("XL"), 40)
+    def test_resta4(self):
         self.assertEqual(roman_to_decimal("XC"), 90)
+    def test_resta5(self):
         self.assertEqual(roman_to_decimal("CD"), 400)
+    def test_resta6(self):
         self.assertEqual(roman_to_decimal("CM"), 900)
     
 if __name__=="__main__":
